0634f7ae400b62d9d6ba13b42d7b1bb55e0e7560
Felipe Cavallo Dobao <dobaofelipe@gmail.com>
2026-03-11 23:06:28 -0300

    colocando os arquivos certos

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int idade;
+    printf("Hello World!\n");
+    printf("Qual a sua idade? ");
+    scanf("%d", &idade);
+    printf("Voce tem %d anos",idade);
+    return 0;
+}
