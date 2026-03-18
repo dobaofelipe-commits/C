11c8c4fd4932775cf25136e6376bdea8baee2374
FelipeCavallo <dobaofelipe@gmail.com>
2026-03-18 18:10:15 -0300

    exercicio dos lados do triangulo

---
+++ exercicio2_2.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+int main(){
+    int x,y,z;
+    printf("Digite o primeiro lado do triângulo");
+    scanf("%d", &x);
+    printf("Digite o segundo lado do triângulo");
+    scanf("%d", &z);
+    printf("Digite o terceiro lado do triângulo");
+    scanf("%d", &y);
+    if(x==z && x==y){
+        printf("Esse triangulo é equilatero");
+    } else if((x==z && x!=y) || (x==y && x!=z) || (y==z)) {
+         printf("Esse triangulo é isosceles");
+    } else {
+        printf("Esse triângulo é escaleno");
+    }
+    getchar();git
+}
